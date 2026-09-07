5dcfc8afe5f46c95db67542ab1d37f9657edf90b
Rishab Raj <rr0948555@gmail.com>
2026-09-07 10:30:47 +0530

    Components In React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
         <button onClick={() => setCount(count+1)}>
             +
         </button>
+        <Header />
     </div>
   )
 }
