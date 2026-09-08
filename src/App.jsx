234ba6529c055fe97a840511097252f4f95b4992
Rishab Raj <rr0948555@gmail.com>
2026-09-08 09:28:50 +0530

    State and Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "./Header";
+import Child from "./Child";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div>
-        <h1>{count}</h1>
-        <button onClick={() => setCount(count+1)}>
-            +
+        <h1>Welcome Alex</h1>
+        <Child title="React.js course"/>
+        <Child title="Next.js course"/>
+
+        <h2>Count: {count}</h2>
+        <button onClick={()=> setCount(count+1)}>
+            Increment
         </button>
-        <Header />
     </div>
   )
 }
---
+++ src/Child.jsx
@@ -0,0 +1,7 @@
+function Child({title}){
+   return(
+       <h2>{title}</h2>
+   )
+}
+
+export default Child;
